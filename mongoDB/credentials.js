56697f8af7426fe76de0c365d9308c076296ef79
mohammed-muddassir <muddassirmud15@gmail.com>
2022-06-16 00:00:01 +0530

    setting up to push to Heroku

--- mongoDB/credentials.js
+++ mongoDB/credentials.js
@@ -2,7 +2,7 @@ if(process.env.NODE_ENV!=='production'){
     require('dotenv').config()
  }
 const {MongoClient} =require('mongodb')
-const uri=process.env.URI;
+const uri=process.env.MONGODB_URI;
 const client=new MongoClient(uri);
 var details;
 module.exports={MongoClient,uri,client};
