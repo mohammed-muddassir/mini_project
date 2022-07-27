a6949ea5b36a0da50508b7029c07de6e5e07d639
mohammed-muddassir <muddassirmud15@gmail.com>
2022-07-27 14:19:59 +0530

    added style and form validation

--- mail/adminMail.js
+++ mail/adminMail.js
@@ -1,10 +1,12 @@
 async function sendAdminMail(details,mailTransport){
-    const message=`Good day Admin!! \n ${details.username} had booked ${details.package}.\n Here are the details of the customer: \n EMAIL : ${details.email} \n PHONE No: ${String(details.phoneno)} \n Reason : ${details.reason} \n TIming : From ${String(details.from)} To ${String(details.to)}`;
+    console.log(String(details.from))
+    const message=`Good day Admin!! \n ${details.username} had booked ${details.package}.\n Here are the details of the customer: \n EMAIL : ${details.email} \n PHONE No: ${String(details.phoneno)} \n Reason : ${details.reason} \n Timing : From ${String(details.from)} To ${String(details.to)}`;
     let mailDetails={
         from:process.env.ADMIN_MAIL,
         to:process.env.ADMIN_MAIL,
         subject:'Confirmation mail',
-        text:message
+        text:message,
+       
     };
     await mailTransport.sendMail(mailDetails,function(err,data){
         if(err){
